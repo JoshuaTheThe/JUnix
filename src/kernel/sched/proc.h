0f9f28103b196cbf8c122af00768ceea57fa896b
Joshua Farmer <rhubarbjosh@outlook.com>
2026-07-11 23:19:29 +0100

    stuff

--- src/kernel/sched/proc.h
+++ src/kernel/sched/proc.h
@@ -10,6 +10,7 @@
 #define MAX_KERNEL_MAPPINGS (65536)
 
 typedef uint64_t pid_t;
+typedef uint64_t tid_t;
 
 typedef struct
 {
@@ -19,19 +20,25 @@ typedef struct
         size_t            capacity;
 } address_space_t;
 
+typedef enum
+{
+        TASK_READY,
+        TASK_RUNNING,
+        TASK_BLOCKED,
+        TASK_DEAD
+} task_state_t;
+
 typedef struct proc_t proc_t;
 
 typedef struct task_t
 {
-        proc_t *parent;
+        void *kernel_stack;
+        struct task_t         *next;
+        proc_t                *parent;
+        tid_t                  tid;
+        task_state_t           state;
         task_state_registers_t regs;
-
-        struct
-        {
-                file_t       **items; // if null, empty
-                size_t         count;
-                size_t         capacity;
-        } fd;
+        uint64_t               last_ran; // in kernel ticks, used for schedule, we have 16 in queue, find oldest and use that when recalculating
 } task_t;
 
 typedef struct proc_t
@@ -45,7 +52,13 @@ typedef struct proc_t
         vnode_t         *cwd;
         task_t          *tasks;
         size_t           taskcount;
-        uint64_t         last_ran; // in kernel ticks, used for schedule, we have 16 in queue, find oldest and use that when recalculating
+
+        struct
+        {
+                file_t       **items; // if null, empty
+                size_t         count;
+                size_t         capacity;
+        } fd;
 
         address_space_t *space;
 } proc_t;
@@ -54,21 +67,28 @@ typedef enum
 {} sig_t;
 
 extern address_space_t kernel_address_space;
-extern proc_t         *schedule_queue[16]; // buffer of things to do, added based on priority, recalculate on empty
+extern task_t         *schedule_queue[16]; // buffer of things to do, added based on priority, recalculate on empty
+extern task_t         *current_task;
+extern proc_t         *current_proc;
 
+void sched_next(void);
+void sched_load(void);
+void sched_save(void);
 void sched_init(void);
 
 /**
  * Returns a new empty process, with a shallow copy of kernel mappings
+ * also 4kb kernel stack
  */
 proc_t *proc_create(void);
+task_t *proc_create_task(proc_t *proc);
 void    proc_kill(proc_t *); // deletes mappings (unless it's shared (kernel is only shared mapping for now)), and removes from ll
 void    proc_suspend(proc_t *); // pause
 void    proc_resume(proc_t *); // resume
 void    proc_clear(proc_t *); // clear all mappings, and registers, also suspend
 void    proc_assign_mappings(proc_t *, mapping_t mappings[], size_t count); // copy these mappings
 void    task_set_regs(task_t *, task_state_registers_t *);
-void    schedule(proc_t *); // add it to the schedule list, automatic
+void    schedule(task_t *); // add it to the schedule list, automatic
 void    proc_recalculate_schedule(void);
 proc_t *proc_find(pid_t);
 
