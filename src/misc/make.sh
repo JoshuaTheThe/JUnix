59356ecc83b7a085c21e97a2317f876181346027
Joshua Farmer <rhubarbjosh@outlook.com>
2026-07-16 15:43:14 +0100

    fuck

--- src/misc/make.sh
+++ src/misc/make.sh
@@ -1,3 +1,4 @@
 
-clang -m32 init.c stdio.c -o init.o -nostdlib -nostartfiles -c -I .
-ld init.o -o init -melf_i386
+clang -m32 init.c -o init.o -nostdlib -nostartfiles -c -I .
+clang -m32 stdio.c -o stdio.o -nostdlib -nostartfiles -c -I .
+ld init.o stdio.o -o init -melf_i386
